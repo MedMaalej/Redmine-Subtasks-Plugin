ae63b5719e549c1dbf7d7712e68d6cf4c949551c
root <maalejmedti@gmail.com>
2016-02-26 11:01:18 +0100

    push

--- app/controllers/subtask_list_columns_controller.rb
+++ app/controllers/subtask_list_columns_controller.rb
@@ -8,32 +8,33 @@ class SubtaskListColumnsController < ApplicationController
     sql = "SELECT id, name FROM projects"
     @projects ||= ActiveRecord::Base.connection.select_all(sql)
     
-    sql = "SELECT id as ident, name FROM custom_fields WHERE type = 'IssueCustomField'"
+    sql = "SELECT  name FROM custom_fields WHERE type = 'IssueCustomField'"
     customFields ||= ActiveRecord::Base.connection.select_all(sql)
     
+    @selectedColumns = SubtasksConfigList.all 
     @allColumns = Constants::DEFAULT_FIELDS + customFields
+   # sql = "SELECT userConfig  FROM subtasks_config_list where projectId=1" 
+   # @allColumns ||= ActiveRecord::Base.connection.select_all(sql) 
     
-    @selectedColumns = SubtaskListColumns.all
-    
-    save = params['save'].blank? ? '' : params['save']
+   # save = params['save'].blank? ? '' : params['save']
 
-    if (save.eql? '1')
-      json = params['selectedColumns'].blank? ? '' : params['selectedColumns']
+    #if (save.eql? '1')
+     # json = params['selectedColumns'].blank? ? '' : params['selectedColumns']
       
-      if(json != '')
-        updateSelectedColumns = JSON.parse(json)     
+     # if(json != '')
+      #  updateSelectedColumns = JSON.parse(json)     
         
-        SubtaskListColumns.delete_all()
+       # SubtasksConfigList.delete_all()
         
-        updateSelectedColumns.each do |col|
-          c = SubtaskListColumns.new
-          c.prj_id = col["prj_id"]
-          c.ident = col["ident"]
-          c.order = col["order"]
-          c.save
+#        updateSelectedColumns.each do |col|
+ #         c = SubtasksConfigList.new
+  #        c.projectId = col["projectId"]
+   #       c.userId = col["userId"]
+    #      c.userConfig = col["userConfig"]
+     #     c.save
           #TODO: do lazy save
-        end   
-      end
-    end                                      
+#        end   
+ #     end
+  #  end                                      
   end  
 end
